c70cb819bcd023caae578becd933a24dd6671e68
Vir-goCode <rdk365@outlook.com>
2022-09-11 08:28:46 -0700

    C programming language

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/** 
+/**
  * main - entry point
  *
- * return:0 if exited correctly, non-zero otherwise
+ * return: always 0 (success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0)
+	return (0);
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * return: 0 always (success)
+ */
+int main(void)
+{
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+        printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+        return (0);
+}	
