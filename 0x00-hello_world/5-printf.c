616288f746e5c8148f83664f6c67b0ecbfe6a884
Vir-goCode <rdk365@outlook.com>
2022-09-11 10:42:11 -0700

    C programming language

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,7 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{  printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+{ 
+ printf("%s\n, "with proper grammar, but the outcome is a piece of art,");
+ return (0);0
 }
