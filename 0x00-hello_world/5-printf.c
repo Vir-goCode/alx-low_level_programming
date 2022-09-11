c1ba12eb8f585bc6ec7865eb94756987be0e8703
Vir-goCode <rdk365@outlook.com>
2022-09-11 10:33:16 -0700

    C programming language

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,9 @@
 /**
  * main - entry point
  *
- * return: always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+{  printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 /**
  * main - entry point
  *
- * return: 0 always (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
@@ -13,10 +12,10 @@ long long int c;
 char d;
 float f;
 
-printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }	
