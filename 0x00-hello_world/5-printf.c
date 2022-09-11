2b2f74a7ff9f4b114828333587e6ac24115ae63e
Vir-goCode <rdk365@outlook.com>
2022-09-11 11:07:00 -0700

    C programming language

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
 /**
  * main - print the string in the put function
  *
- * Description: using the main function, this program  prints "With proper gramm   ar, but the outcome is a piece of art
+ * Description: using the main function, this program  prints
+ * with proper grammar, but the outcome is a piece of art
  * Return: Always 0 (Success)
  */
 int main(void)
