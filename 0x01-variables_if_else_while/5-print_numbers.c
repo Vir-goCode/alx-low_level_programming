7ff404d7782d39e214ba2b58f4eb6391f9579b9d
Vir-goCode <rdk365@outlook.com>
2022-09-12 14:47:19 -0700

    Variables-if-else-while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers of base
+ * 10 starting from zero followed by a new line
+ * Return: 0
+ */
+int main(void)
+{int i;
+for (i = 0; i < 10; i++)
+{
+printf("%i"; i);
+}
+printf('\n');
+return (0);
+}
