1cfa0fc2e271e83c7353ad7ed5beb07fa5812b36
Vir-goCode <rdk365@outlook.com>
2022-09-12 14:52:09 -0700

    Variables-if-else-while

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,11 +5,12 @@
  * Return: 0
  */
 int main(void)
-{int i;
-for (i = 0; i < 10; i++)
 {
-printf("%i"; i);
+int n;
+for (n = 0; n < 10; n++)
+{
+printf("%d"; n);
 }
-printf('\n');
+printf("\n");
 return (0);
 }
