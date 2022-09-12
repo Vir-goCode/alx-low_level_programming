dea13299ef8b78beb9da100510f0e333a6566092
Vir-goCode <rdk365@outlook.com>
2022-09-12 16:33:33 -0700

    Variables-if-else-while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,12 +10,12 @@ int main(void)
 int c;
 for (c = 48; c <= 58; c++)
 {
-	putchar(c);
-	if (c !=58)
-	{
-		putchar(',');
-		putchar(' ');
-	}
+putchar(c);
+if (c !=58)
+{
+putchar(',');
+putchar(' ');
+}
 }
 putchar('\n')
 return (0);
