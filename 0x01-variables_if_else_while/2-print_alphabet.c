ffbd5e78ffda72b99e8e8f151c1169aa2dcecfef
Vir-goCode <rdk365@outlook.com>
2022-09-12 13:35:40 -0700

    Variables-if-else-while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet in lowercase
+ * Description: using the main function
+ *Return: 0
+ */
+int main(void)
+{char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+	putchar (ch);
+	}
+	putchar ('\n');
+	return (0);
+}
