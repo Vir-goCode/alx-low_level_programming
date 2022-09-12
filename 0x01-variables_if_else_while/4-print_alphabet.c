53145196a3068040656dceaeb2b2ec4cbebcbf34
Vir-goCode <rdk365@outlook.com>
2022-09-12 14:26:15 -0700

    Variables-if-else-while

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ * followed by a new line except q and e
+ * Return: 0
+ */
+int main(void)
+{char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch != 'e' && ch != 'q')
+{
+putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}
