5384514f55305768ed4603c72ddd890ebcdc6c86
Vir-goCode <rdk365@outlook.com>
2022-09-12 15:10:04 -0700

    Variables-if-else-while

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- * main - print all single digit numbers of base
- * 10 starting from zero followed by a new line
- * Return: 0
- */
+*main - prints all single digit numbers of base 10
+*starting from 0, followed by a new line
+Return: 0
+*/
 int main(void)
 {
 int n;
 for (n = 0; n < 10; n++)
 {
-    printf("%d"; n);
+printf("%d", n);
 }
 printf("\n");
 return (0);
