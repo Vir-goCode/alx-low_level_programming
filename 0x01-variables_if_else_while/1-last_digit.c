e6bc7bf20f4d090b24e560c89f759045e3dbeb4d
Vir-goCode <rdk365@outlook.com>
2022-09-12 17:25:13 -0700

    Variable-if-else-while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,8 +4,7 @@
 /**
 *main - prints if the number is positive, zero, or negative
 *Description: using the main function
-*this program prints if "Programming is positive, zero, or negative
-*Return: 0
+*Return: Always 0 (Success)
 */
 int main(void)
 {int n;
@@ -21,7 +20,7 @@ else if (l == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, l);
 }
-else() 
+else 
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 }
