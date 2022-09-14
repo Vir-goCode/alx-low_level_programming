6f894ccaf46a05059ba78bc99ee8a37e115f21fa
Vir-goCode <rdk365@outlook.com>
2022-09-14 14:30:58 -0700

    functions-nested-loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,15 +10,16 @@ void print_alphabet_x10(void)
 char ch;
 int  i;
 i = 0;
+
 while (i < 10)
 {
-ch = 'a';
+     ch = 'a';
 while (ch <= 'z')
 {
-_putchar(ch);
-ch++;
-}
-_putchar('\n')
-i++;
-}
+        _putchar(ch);
+        ch++;    
+    }
+       _putchar('\n')
+        i++;
+    }
 }
