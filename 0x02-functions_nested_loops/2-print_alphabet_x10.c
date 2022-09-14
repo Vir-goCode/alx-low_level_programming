723ed06626e8061fadcf6b144a63537e94f8e0f5
Vir-goCode <rdk365@outlook.com>
2022-09-14 14:27:13 -0700

    functions-nested-loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,21 +3,22 @@
 /**
  *print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  *followed by a new line
+ *Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
 char ch;
-int i;
+int  i;
 i = 0;
 while (i < 10)
 {
 ch = 'a';
-while (ch <= 'z')													
+while (ch <= 'z')
 {
 _putchar(ch);
-Ch++;
+ch++;
 }
-_putchar('\n');
+_putchar('\n')
 i++;
 }
 }
