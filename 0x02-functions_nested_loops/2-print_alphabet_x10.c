ef49fbc7550ea9208792bd2c16630b31c6e0e46c
Vir-goCode <rdk365@outlook.com>
2022-09-14 14:38:24 -0700

    functions-nested-loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,25 +1,14 @@
 #include "main.h"
-
 /**
- *print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
- *followed by a new line
- *Return: Always 0 (Success)
+ * print_alphabet - functions to print abc
+ * Return: 0
  */
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
-char ch;
-int  i;
-i = 0;
-
-while (i < 10)
-{
-     ch = 'a';
-while (ch <= 'z')
-{
-        _putchar(ch);
-        ch++;    
-    }
-       _putchar('\n')
-        i++;
-    }
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n);
 }
