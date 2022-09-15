a96756ac0efc40ac9f87205a062ff8346d5f28b3
Vir-goCode <rdk365@outlook.com>
2022-09-15 13:45:02 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *main - tests function that prints if the interger is positive or negative.
+ *main - tests function that prints if the interger is positive or negative
  *Return: 0
  */
 int main(void)
@@ -9,5 +9,6 @@ int i;
 
 i = 0;
 positive_or_negative(i);
+
 return (0);
 }
