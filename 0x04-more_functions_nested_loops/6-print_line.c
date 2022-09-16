020a314c276cf025026042ef8ae43b32d4add9d4
Vir-goCode <rdk365@outlook.com>
2022-09-16 15:26:14 -0700

    more-functions-nested-loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: parameter
+ * Return: nothing
+ */
+
+void print_line(int n)
+{
+whilw (n-- > 0)
+{
+putchar('_');
+}
+putchar('\n');
+}
