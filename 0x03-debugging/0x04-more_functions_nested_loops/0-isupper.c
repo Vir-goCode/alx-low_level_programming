7cf731b664ca346f8a2f339418c849467ab08909
Vir-goCode <rdk365@outlook.com>
2022-09-16 10:26:57 -0700

    more-functions-nested-loops

--- 0x03-debugging/0x04-more_functions_nested_loops/0-isupper.c
+++ 0x03-debugging/0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _isupper - checks for uppercase character
@@ -9,7 +8,10 @@
 
 int _isupper(int c)
 {
-if (c >= 65 && c <= 90)
+return (c >= 'A' && c <= 'Z');
+{
 return (1);
+}
+
 return (0);
 }
