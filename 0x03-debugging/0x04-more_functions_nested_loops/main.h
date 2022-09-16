d581c8448d0f7bb032cbe919c21c6e13405b33eb
Vir-goCode <rdk365@outlook.com>
2022-09-16 10:08:41 -0700

    more-functions-nested-loops

--- 0x03-debugging/0x04-more_functions_nested_loops/main.h
+++ 0x03-debugging/0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,12 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdio.h>
+/**
+ * main - header file for prototypes
+ * void prototypes (void)
+ * int prototypes (int)
+ */
 
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -15,5 +15,9 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 
-#endif
+#endif /* MAIN */
