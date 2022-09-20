ee0eea90877023dfd3bd62fb529eb563dea8d70a
Vir-goCode <rdk365@outlook.com>
2022-09-20 13:42:44 -0700

    pointers-arrays-strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: string to reverse
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+int i = 0;
+
+int j = 0;
+
+char temp[1000];
+
+while (s[i] != '\0')
+{
+i++;
+}
+for (; i > 0; i--)
+{
+temp[j] = s[i - 1];
+j++;
+}
+while (i < j)
+{
+s[i] = temp[i];
+i++;
+}
+}
