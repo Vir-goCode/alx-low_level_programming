da4e9b580ccc1c59d9f22711cb58f4bee33cce10
Vir-goCode <rdk365@outlook.com>
2022-09-20 14:10:49 -0700

    pointers-arrays-strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,26 +8,21 @@
 
 void puts_half(char *str)
 {
-int len, n, i;
-len = 0;
+int i, j;
+i = 0;
 
-while (str[len] != '\0')
+while (str[i] != '\0')
+i++;
+
+j = i / 2;
+
+if (i % 2 == 1)
+j++;
+
+while (j < i)
 {
-len++;
-}
-if (len % 2 == 0)
-{
-for (i = len / 2; str[i] != '\0'; i++)
-{
-_putchar(str[i]);
-}
-}
-else if (len % 2)
-{
-for (n = (len - 1) / 2; n < len - 1; n++)
-{
-_putchar(str[n + 1]);
+_putchar(str[j]);
+j++;
 }
 _putchar('\n');
-}
 }
