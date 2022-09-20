ef29033145beab8353e3c6eb86e1c3d95802688d
Vir-goCode <rdk365@outlook.com>
2022-09-20 14:24:21 -0700

    pointers-arrays-strings

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - print n digits of an array
+ * @a: array to print
+ * @n: number of values to print
+ *  Return: nothing
+ */
+
+void print_array(int *a, int n)
+{
+int inc;
+int inc;
+
+for (inc = 0; inc < n ; inc++)
+{if (inc != n - 1)
+printf("%d, ", a[inc]);
+else
+printf("%d" , a[inc]);
+}
+putchar(10);
+}
