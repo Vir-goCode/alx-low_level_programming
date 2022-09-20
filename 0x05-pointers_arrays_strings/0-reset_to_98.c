12ac53bc92e306ca22870e31a47e4e466597d659
Vir-goCode <rdk365@outlook.com>
2022-09-20 11:50:34 -0700

    pointers-arrays-strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,18 +2,11 @@
 
 /**
 * reset_to_98 - set value of int to 98
-* @n: address of value to change
-* Description: resets *n to 98
-* Return: always 0 (Success)
+* @n: parameter
+* Return: nothing
 */
 
 void reset_to_98(int *n)
 {
-int n;
-n = 402
-
-printf("n=%d\n", n);
-reset_to_98(&n);
-printf("n=%d\n", n);
-return (0);
+*n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_h
 
+/**
+ * void prototypes()
+ * int prototypes()
+ */
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -14,4 +19,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif /* MAIN_H */
+#endif /*MAIN_H*/
