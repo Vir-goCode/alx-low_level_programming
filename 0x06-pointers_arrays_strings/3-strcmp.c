e84854a1397cea9ec243e91222901bb5ae4f02de
Vir-goCode <rdk365@outlook.com>
2022-09-22 14:55:52 -0700

    more-pointers-arrays-strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,19 +10,20 @@
 int _strcmp(char *s1, char *s2)
 {
 int i;
-
 i = 0;
 
 while (s1[i] != '\0' && s2[i] != '\0')
 {
 if (s1[i] != s2[i])
-
+{
 return (s1[i] - s2[i]);
 i++;
 }
 if (s1[i] == s2[i])
-
+{
 return (0);
 
 return (s1[i] - s2[i]);
+}
+}
 }
