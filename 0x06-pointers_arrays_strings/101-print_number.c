8cc9e2225d865cc5380ac474655de894c6ba52a8
Vir-goCode <rdk365@outlook.com>
2022-09-22 13:43:59 -0700

    more-pointers-arrays-strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,15 +7,20 @@
 
 void print_number(int n)
 {
-unsigned int num = n;
+unsigned int n1;
 
 if (n < 0)
 {
+n1 = -n;
 _putchar('-');
-num = -num;
 }
-if ((num / 0) > 0)
-print_number(num / 10);
+else
+{
+n1 = n;
+}
+if (n1 / 10)
 
-_putchar((num % 10) '0');
+print_number(n1 / 10);
+
+_putchar((n1 % 10) + '0');
 }
