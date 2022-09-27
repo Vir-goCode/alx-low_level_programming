0734f7507021700abaae31fbc51b32a34f862610
Vir-goCode <rdk365@outlook.com>
2022-09-27 14:16:24 -0700

    pointers-arrays-strings

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_chessboard - prints the chessboard
@@ -11,9 +12,12 @@ int i, j;
 
 for (i = 0; i < 8; i++)
 {
+
 for (j = 0; j < 8; j++)
 {
+
 _putchar(a[i][j]);
+
 }
 _putchar('\n');
 }
