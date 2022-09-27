d0cada0abc2511cea1c522c9edda4b3960f6284b
Vir-goCode <rdk365@outlook.com>
2022-09-27 12:35:50 -0700

    pointers-arrays-strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * void prototypes()
+ * int prototypes()
+ */
+
+int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -10,6 +16,6 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-int putchar(char c);
+
 
 #endif /*MAIN_H*/
