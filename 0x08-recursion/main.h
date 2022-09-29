71ce0ce777283944398b3fb8773d174c5e18c675
Vir-goCode <rdk365@outlook.com>
2022-09-29 13:15:27 -0700

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 
 /**
@@ -7,7 +8,7 @@
  * int prototypes()
  */
 
-int _putchar(char c)
+int _putchar(char)
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
