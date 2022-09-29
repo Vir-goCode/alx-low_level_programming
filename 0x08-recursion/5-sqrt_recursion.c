e7a866f1d2472e82ada5adbe89d1c983cb206a1d
Vir-goCode <rdk365@outlook.com>
2022-09-29 13:50:02 -0700

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,6 +14,7 @@ int _sqrt_recursion(int n)
 {
 if (n < 0)
 return (-1);
+else
 return (actual_sqrt_recursion(n, 0));
 }
 
@@ -31,5 +32,6 @@ if (i * i > n)
 return (-1);
 if (i * i == n)
 return (i);
+else
 return (actual_sqrt_recursion(n, i + 1));
 }
