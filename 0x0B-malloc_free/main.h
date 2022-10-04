8a7c651c1254091eec7ad284dfcd0c0399381b76
Vir-goCode <rdk365@outlook.com>
2022-10-04 14:05:53 -0700

    malloc-free

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,13 +1,15 @@
 #ifndef "MAIN_H"
 #define "MAIN_H"
 
+#include <stdlib.h>
+
 /**
  * char prototypes()
  * int prototypes()
  * void prototypes()
  */
 
-int _putchar(char c)
+int _putchar(char);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
