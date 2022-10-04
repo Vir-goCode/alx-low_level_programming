b7e2db0c2178233350a21b8315d91e1d2bdc1eea
Vir-goCode <rdk365@outlook.com>
2022-10-04 15:04:05 -0700

    malloc-free

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * argstostr - concatenates all the arguments of the program
+ * @ac: number of arguments
+ * @av: array of arguments
+ *
+ * Return: Pointer to the new string (Success), NULL (Error)
+ */
+
+char *argstostr(int ac, char **av)
+{
+int r, d, k, len;
+char *str;
+
+if (ac == 0 || av == NULL)
+return (NULL);
+
+for (r = 0; r < ac; r++)
+{
+for (d = 0; av[r][d] != '\0'; d++)
+len++;
+len++;
+}
+str = malloc(sizeof(char) * (len + 1));
+
+if (str == NULL)
+return (NULL);
+
+k = 0;
+
+for (r = 0; r < ac; r++)
+{
+for (r = 0; av[r][d] != '\0'; d++)
+str[k] = av[r][d];
+k++;
+}
+str[k] = '\n';
+k++;
+}
+return (str);
+}
