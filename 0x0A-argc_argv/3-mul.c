3fc4c98fe5036cf9fdbc4d9e31f64896c84ddf8d
Vir-goCode <rdk365@outlook.com>
2022-10-05 11:46:31 -0700

    argc-argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 
 /**
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: 0 (Success), 1 (Error)
+ * Return: 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
 int result, num1, num2;
 
-if (argc < 3 || argc > 3)
-{
+if (arg != 3)
 printf("Error\n");
-return (1);
-}
 
+else
+{
 num1 = _atoi(argv[1]);
 num2 = _atoi(argv[2]);
 result = num1 * num2;
-
 printf("%d\n", result);
+}
 return (0);
 }
