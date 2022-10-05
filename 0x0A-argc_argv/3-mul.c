32c6ec0e58617d22d9c95abe8fe0addcb207de39
Vir-goCode <rdk365@outlook.com>
2022-10-05 12:04:27 -0700

    argc-argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,27 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * main - multiplies two numbers
+ * main - multiply two numbers
  * @argc: number of arguments
- * @argv: array of arguments
+ * @argv: arguments entered
+ * Description: return product
  *
- * Return: 0 (Success)
+ * Return: zero
  */
 
 int main(int argc, char *argv[])
 {
-int num_1, num_2, mul;
+int num1, num2;
+char *s1 = argv[1];
+char *s2 = argv[2];
 
-if (arg != 3)
-printf("Error\n");
+num1 = num2 = 0;
 
-else
+if (argc != 3)
 {
-num_1 = _atoi(argv[1]);
-num_2 = _atoi(argv[2]);
-mul = num_1 * num_2;
-printf("%d\n", mul);
+printf("Error\n");
+return (1);
 }
+
+if (s1[0] == '-')
+num1 = -1 * atoi(&s1[1]);
+else
+num1 = atoi(&s1[0]);
+if (s2[0] == '-')
+num2 = -1 * atoi(&s2[1]);
+else
+num2 = atoi(&s2[0]);
+printf("%d\n", num1 * num2);
 return (0);
 }
