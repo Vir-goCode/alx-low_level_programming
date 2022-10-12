2222676e5cde10dacd3b68dfdbe96bb8c39bc664
Vir-goCode <rdk365@outlook.com>
2022-10-11 20:20:12 -0700

    structures-typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,11 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
  * struct dog - a dog's basic info
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
  */
 
 struct dog
@@ -26,4 +26,5 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+
 #endif
