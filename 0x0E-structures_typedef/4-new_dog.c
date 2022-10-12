dfec62ff0dd9296237f5d4bca1c72e842858fef9
Vir-goCode <rdk365@outlook.com>
2022-10-11 20:58:16 -0700

    structures-typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * new_dog - creates dog object
@@ -11,8 +10,34 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-if (hobbes == NULL)
+dog_t *dog;
+int len1, len2;
+
+len1 = _strlen(name);
+len2 = _strlen(owner);
+
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
 return (NULL);
 
-return (hobbes);
+dog->name = malloc(sizeof(char) * (len1 + 1));
+if (dog->name == NULL)
+{
+free(dog);
+return (NULL);
+}
+
+dog->owner = malloc(sizeof(char) * (len2 + 1));
+if (dog->owner == NULL)
+{
+free(dog);
+free(dog->name);
+return (NULL);
+}
+
+_strcpy(dog->name, name);
+_strcpy(dog->owner, owner);
+dog->age = age;
+
+return (dog);
 }
