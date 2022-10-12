10789eb00e61343cdcb3b3986febe3ec7c6a437a
Vir-goCode <rdk365@outlook.com>
2022-10-11 20:33:29 -0700

    structures-typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - create dog in function
+ * leash: dog object
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: dog, NULL if fails
+ */
+
+void init_dog(struct dog *leash, char *name, float age, char *owner)
+{
+if (hobbes == NULL)
+return;
+leash->name = name;
+leash->age = age;
+leash->owner = owner;
+}
