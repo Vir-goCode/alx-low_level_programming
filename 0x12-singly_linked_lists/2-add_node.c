68256db7b31c5b8e57203d222c4a743509a80923
Vir-goCode <rdk365@outlook.com>
2022-10-21 12:52:30 -0700

    sinly-linked-lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,30 +1,42 @@
+#include "lists.h"
 #include <stdio.h>
 #include <stddef.h>
 #include <string.h>
-#include "lists.h"
+
+/**
+ * _strnlen - returns the length of a string
+ * @s: string to count
+ * Return: lenth of string
+ */
+
+
+
+int _strnlen(const char *s)
+{
+int i;
+
+if (s == NULL)
+return (0);
+for (i = 0; s[i] != '\0'; i++);
+return (i);
+}
 
 /**
  * add_node - adds a new node at the beginning of a linked list
  * @head: double pointer to the list_t list
  * @str: new string to add in the node
- *
  * Return: the address of the new element, or NULL if it fails
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *new;
-unsigned int len = 0;
-
-while (str[len])
-len++;
-if (!new)
+new = malloc(sizeof(list_t));
+if (new == NULL)
 return (NULL);
 
 new->str = strdup(str);
-new->len = len;
-new->next = (*head);
-(*head) = new;
-
-return (*head);
+new->len = _strnlen(str);
+new->next = *head;
+*head = new;
+return (new);
 }
