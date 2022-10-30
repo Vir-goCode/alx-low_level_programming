649b4a758a40ff639af38f356fba3eec8a576c21
Vir-goCode <rdk365@outlook.com>
2022-10-30 11:30:12 -0700

    bit-manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,71 +1,31 @@
-#include "holberton.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
- * power - returns 2 ^ y
- * @c: only calculate if c != 0
- * @y: exponent value
- * Return: 2 ^ y or 0 if fail
- */
-
-int power(char c, int y)
-{
-if (c == '0')
-return (0);
-
-else if (y == 0)
-return (1);
-
-else if (y == 1)
-return (2);
-
-return (2 * power(c, y - 1));
-}
-
-/**
- * binary - converts a string binary number into a unsigned int
- * @b: binary string
- * @num: pointer to number where value is stored
- * Return: decimal as unsigned int
- */
-
-unsigned int binary(const char *b, unsigned int *num)
-{
-int index;
-
-if (b[1] == '\0')
-{
-*num = b[0] - '0';
-return (1);
-}
-
-index = binary(b + 1, num);
-*num +=  power(b[0], index);
-return (index + 1);
-}
-
-/**
- * binary_to_uint - converts a string binary number into a unsigned int
- * @b: binary string
- * Return: decimal as unsigned int
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointing to a string of 0 and 1 chars.
+ * Return: the converted number (sucess), or 0 (error).
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int num, i;
+unsigned int sum = 0;
+unsigned int power = 1;
+int i = 0;
 
-num = 0;
-
-if (b == NULL || b[0] == '\0')
+if (!b)
 return (0);
+while (b[i])
+i++;
+i--;
 
-for (i = 0; b[i] != '\0'; i++)
+while (i >= 0)
 {
-if (b[i] != '1' && b[i] != '0')
+if (b[i] != '0' && b[i] != '1')
 return (0);
+if (b[i] == '1')
+sum += power;
+i--;
+power += power;
 }
-
-binary(b, &num);
-return (num);
+return (sum);
 }
